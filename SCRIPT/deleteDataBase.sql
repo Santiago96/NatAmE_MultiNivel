fbaf4d7cf9c64ce42353075b706a82e7163baaff
bpjs159 <js.bonilla@hotmail.com>
2018-04-05 01:56:54 -0500

    Script de base de datos3

---
+++ SCRIPT/deleteDataBase.sql
@@ -0,0 +1,16 @@
+drop user multinivel cascade;
+drop user s1018485092 cascade;
+drop user a1005855865 cascade;
+drop user j1016091072 cascade;
+drop user p1023944061 cascade;
+drop user j1018492522 cascade;
+
+
+drop tablespace DEFMULTINIVEL;
+drop tablespace DEFRMUNDO;
+drop tablespace TEMRMULTINIVEL;
+
+
+drop role R_MULTINIVEL;
+drop role R_CLIENTE;
+drop role R_REPRESENTANTEVENTAS;
