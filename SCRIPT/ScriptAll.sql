ff23f50ed2380e563cece05a8806514fdc7b33b8
bpjs159 <js.bonilla@hotmail.com>
2018-04-05 01:34:25 -0500

    Comentarios de Scripts y llenado2

--- SCRIPT/ScriptAll.sql
+++ SCRIPT/ScriptAll.sql
@@ -2,6 +2,7 @@ start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\Multinivel Master\NatAmE_MultiNivel\SCRIPT\Seguridad\Multinivel.sql";
 conn multinivel/finalbases
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\Multinivel Master\NatAmE_MultiNivel\SCRIPT\ScriptModeloBD.sql";
+start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\Multinivel Master\NatAmE_MultiNivel\SCRIPT\ScriptLlenado.sql"
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\Multinivel Master\NatAmE_MultiNivel\SCRIPT\Seguridad\Sinonimos.sql";
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\Multinivel Master\NatAmE_MultiNivel\SCRIPT\Seguridad\Vistas.sql";
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\Multinivel Master\NatAmE_MultiNivel\SCRIPT\Seguridad\Schema.sql";
