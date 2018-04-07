1e77c4cf3ed141ae0bed60d55bdeb3c66b4a972c
Santiago96 <santicollazosbarrera@hotmail.com>
2018-04-07 12:40:59 -0500

    llenado
    llenado

--- SCRIPT/ScriptLlenado.sql
+++ SCRIPT/ScriptLlenado.sql
@@ -2,14 +2,10 @@ start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\REGION.sql";
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\CIUDAD.sql";
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\BANCO.sql";
-start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\ESTADOVENTA.sql";
-start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\CARGO.sql";
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\TIPOCONTACTO.sql";
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\TARJETA.sql";
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\TIPOREPRESENTANTE.sql";
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\CATEGORIA.sql";
-start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\SUBCATEGORIA.sql";
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\PERSONA.sql";
-start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\STOCK.sql";
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\PRODUCTO.sql";
 start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Llenado\ITEM.sql";
