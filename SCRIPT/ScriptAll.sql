8bc01b21544b6fc3dc1b7a685ecc4feeee03a622
Santiago96 <santicollazosbarrera@hotmail.com>
2018-04-08 21:04:08 -0500

    Script
    actualizacion script

--- SCRIPT/ScriptAll.sql
+++ SCRIPT/ScriptAll.sql
@@ -1,12 +1,12 @@
-start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Seguridad\Tablespace.sql";
-start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Seguridad\Multinivel.sql";
+start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Tablespace.sql";
+start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Multinivel.sql";
 conn multinivel/finalbases
-start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\ScriptModeloBD.sql";
-start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\ScriptLlenado.sql"
-start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Seguridad\Sinonimos.sql";
-start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Seguridad\Vistas.sql";
-start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Seguridad\Schema.sql";
-conn system/123456
-start "C:\Users\jsbon\Dropbox\Universidad Distrital\Academia\9. Noveno Semestre\Bases de Datos II\NatAmE_MultiNivel\SCRIPT\Seguridad\Roles.sql";
+start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\ScriptModeloBD.sql";
+start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\ScriptLlenado.sql"
+start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Sinonimos.sql";
+start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Vistas.sql";
+start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Schema.sql";
+conn system/HESOYAMYASNAEB
+start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Roles.sql";
 
 
