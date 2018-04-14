ba5bed9e40bb7d2b299c7051ec482b900b3800a5
Santiago96 <santicollazosbarrera@hotmail.com>
2018-04-14 16:41:43 -0500

    acutalizacion dao
    dao

--- SCRIPT/ScriptAll.sql
+++ SCRIPT/ScriptAll.sql
@@ -4,7 +4,7 @@ conn multinivel/finalbases
 start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\ScriptModeloBD.sql";
 start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\ScriptLlenado.sql"
 start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Sinonimos.sql";
-conn system/HESOYAMYASNAEB
+conn system/
 start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Admin.sql";
 conn f1005855668/finalbases
 start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Schemas.sql";
