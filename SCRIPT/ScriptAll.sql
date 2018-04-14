153d10a635fc0dad04f69b84ba3745b7b68d1040
Santiago96 <santicollazosbarrera@hotmail.com>
2018-04-14 10:04:22 -0500

    Actualizacion llenado
    llenado actualizado

--- SCRIPT/ScriptAll.sql
+++ SCRIPT/ScriptAll.sql
@@ -2,15 +2,13 @@ start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\S
 start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Multinivel.sql";
 conn multinivel/finalbases
 start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\ScriptModeloBD.sql";
---start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\ScriptLlenado.sql"
+start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\ScriptLlenado.sql"
 start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Sinonimos.sql";
-
-conn system/rexilo
+conn system/HESOYAMYASNAEB
 start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Admin.sql";
-
 conn f1005855668/finalbases
 start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Schemas.sql";
 start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Roles.sql";
---start "C:\Users\Santiago\Documents\NetBeansProjects\AuxBases\NatAmE_MultiNivel\SCRIPT\Seguridad\Roles.sql";
+
 
 
