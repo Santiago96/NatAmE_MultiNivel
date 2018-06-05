cd9e0742e09bce0ff1ae108ed92e6ccf0c7af598
Olixer <olix3r@gmail.com>
2018-06-04 19:10:47 -0500

    Actualizaciòn

--- SCRIPT/CrearTodo.sql
+++ SCRIPT/CrearTodo.sql
@@ -5,8 +5,14 @@ start "C:\Users\jsbon\Dropbox\Proyectos\NatAmE_MultiNivel\SCRIPT\ScriptModeloBD.
 start "C:\Users\jsbon\Dropbox\Proyectos\NatAmE_MultiNivel\SCRIPT\ScriptLlenado.sql";
 start "C:\Users\jsbon\Dropbox\Proyectos\NatAmE_MultiNivel\SCRIPT\Seguridad\Vistas.sql";
 start "C:\Users\jsbon\Dropbox\Proyectos\NatAmE_MultiNivel\SCRIPT\Seguridad\Sinonimos.sql";
---start "C:\Users\cathe\Desktop\Abril\BD II\NatAmE_MultiNivel\SCRIPT\PL_SQL\Package\pk_procedimientos.hed.hed";
---start "C:\Users\cathe\Desktop\Abril\BD II\NatAmE_MultiNivel\SCRIPT\PL_SQL\Package\pk_procedimientos.bod";
+
+conn / as sysdba;
+GRANT EXECUTE ON SYS.utl_file TO multinivel;
+
+conn multinivel/finalbases;
+
+start "C:\Users\jsbon\Dropbox\Proyectos\NatAmE_MultiNivel\SCRIPT\PL_SQL\Package\pk_procedimientos.hed.hed";
+start "C:\Users\jsbon\Dropbox\Proyectos\NatAmE_MultiNivel\SCRIPT\PL_SQL\Package\pk_procedimientos.bod";
 
 conn system/123456
 start "C:\Users\jsbon\Dropbox\Proyectos\NatAmE_MultiNivel\SCRIPT\Seguridad\Admin.sql";
--- SCRIPT/CrearTodoSantiago.sql
+++ SCRIPT/CrearTodoSantiago.sql
@@ -5,6 +5,16 @@ start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\ScriptModeloB
 start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\ScriptLlenado.sql";
 start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\Seguridad\Vistas.sql";
 start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\Seguridad\Sinonimos.sql";
+
+conn / as sysdba;
+GRANT EXECUTE ON SYS.utl_file TO multinivel;
+
+conn multinivel/finalbases;
+
+start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\PL_SQL\Package\pk_procedimientos.hed.hed";
+start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\PL_SQL\Package\pk_procedimientos.bod";
+create or replace directory dir_temp as 'C:\temp';
+	
 conn system/HESOYAMYASNAEB
 start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\Seguridad\Admin.sql";
 conn f1005855668/finalbases
