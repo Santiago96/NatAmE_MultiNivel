a7ef4a4b2e4823178bdab168cb0b10e1a40ee638
Santiago Collazos Barrera <santicollazosbarrera@hotmail.com>
2018-06-04 23:36:54 -0500

    crear
    crear

--- SCRIPT/CrearTodoSantiago.sql
+++ SCRIPT/CrearTodoSantiago.sql
@@ -5,18 +5,28 @@ start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\ScriptModeloB
 start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\ScriptLlenadoSantiago.sql";
 start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\Seguridad\Vistas.sql";
 start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\Seguridad\Sinonimos.sql";
+create or replace directory DIR_TMP as 'C:\temp';
+
 
 conn / as sysdba;
-GRANT EXECUTE ON SYS.utl_file TO multinivel;
+GRANT EXECUTE ON SYS.utl_file TO multinivel;	
+
 
 conn multinivel/finalbases;
 
 start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\PL_SQL\Package\pk_procedimientos.hed.hed";
 start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\PL_SQL\Package\pk_procedimientos.bod";
-create or replace directory DIR_TMP as 'C:\temp';
+
 	
 conn system/HESOYAMYASNAEB
 start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\Seguridad\Admin.sql";
 conn f1005855668/finalbases
 start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\Seguridad\Schemas.sql";
 start "C:\Users\Santiago\Documents\GitHub\NatAmE_MultiNivel\SCRIPT\Seguridad\Roles.sql";
+
+
+conn / as sysdba;
+GRANT EXECUTE ON SYS.utl_file TO r_cliente;
+GRANT EXECUTE ON SYS.utl_file TO r_representanteventas;
+GRANT read, write on directory DIR_TMP to r_cliente;
+GRANT read, write on directory DIR_TMP to r_representanteventas;
